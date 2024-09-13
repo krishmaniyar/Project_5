4b91d07b2ea2240981a95e47316d8e5b1245f54b
krish <krishmaniyar27@gmail.com>
2024-09-13 15:23:34 +0530

    index.html

--- javascript.js
+++ javascript.js
@@ -1,133 +1,93 @@
-let input1=-1;
-let input2;
-let input3;
+var input1 = "";
+var input2 = "";
+var input3 = "";
 
 var textbox = document.getElementById('textbox');
 var answer ="";
+
 function operate()
 {
     if(input2 == "+")
     {
         answer = addition(input1,input3);
     }
-    if(input2 == "-")
+    else if(input2 == "-")
     {
         answer = subtraction(input1,input3);
     }
-    if(input2 == "/")
+    else if(input2 == "/")
     {
         answer = division(input1,input3);
     }
-    if(input2 == "*")
+    else if(input2 == "*")
     {
         answer = multiplication(input1,input3);
     }
+    else if(input2 == "%")
+    {
+        answer = percentage(input1,input3);
+    }
+    else
+    {
+        answer = "NaN";
+    }
     textbox.value = answer;
-    input1=-1;
+    input1="";
+    input2="";
+    input3="";
 }
-
-const container = document.getElementById("container");
 
 function clear1()
 {
-    input1=-1;
-    input2="+";
-    input3=0;
+    input1="";
+    input2="";
+    input3="";
     textbox.value = "";
 }
 
-function makeCal(rowNum,colNum)
+function delete1()
 {
-    var count = 7;
-    let newCol1 = document.createElement("div");
-    container.appendChild(newCol1).className = "grid-column";
-    let newCell4 = document.createElement("div");
-    newCell4.innerHTML = "<button onclick='operate()'>(</button>";
-    newCol1.appendChild(newCell4).className = "grid-item";
-    let newCell5 = document.createElement("div");
-    newCell5.innerHTML = "<button onclick='operate()'>)</button>";
-    newCol1.appendChild(newCell5).className = "grid-item";
-    let newCell6 = document.createElement("div");
-    newCell6.innerHTML = "<button onclick='operate()'>%</button>";
-    newCol1.appendChild(newCell6).className = "grid-item";
-    let newCell7 = document.createElement("div");
-    newCell7.innerHTML = "<button onclick='clear1()'>AC</button>";
-    newCol1.appendChild(newCell7).className = "grid-item";
-    for(i=0;i<colNum;i++)
+    textbox.value = textbox.value.slice(0,-1);
+    if(input3!="")
     {
-        let newCol = document.createElement("div");
-        container.appendChild(newCol).className = "grid-column";
-        for(j=0;j<rowNum;j++)
-        {
-            let newCell = document.createElement("div");
-            newCell.innerHTML = "<button onclick='values(" + Number(count+j) +")'>" + Number(count+j)+ "</button>";
-            newCol.appendChild(newCell).className = "grid-item";
-            if(i==0 && j==2)
-            {
-                let newCell2 = document.createElement("div");
-                newCell2.innerHTML = "<button onclick='operators(3)'>/</button>";
-                newCol.appendChild(newCell2).className = "grid-item";
-            }
-            if(i==1 && j==2)
-            {
-                let newCell2 = document.createElement("div");
-                newCell2.innerHTML = "<button onclick='operators(2)'>*</button>";
-                newCol.appendChild(newCell2).className = "grid-item";
-            }
-            if(i==2 && j==2)
-            {
-                let newCell2 = document.createElement("div");
-                newCell2.innerHTML = "<button onclick='operators(-)'>-</button>";
-                newCol.appendChild(newCell2).className = "grid-item";
-            }
-        }
-        count-=3;
+        input3 = input3.slice(0,-1);
     }
-    let newCol = document.createElement("div");
-    container.appendChild(newCol).className = "grid-column";
-    let newCell = document.createElement("div");
-    newCell.innerHTML = "<button onclick='operate()'>0</button>";
-    newCol.appendChild(newCell).className = "grid-item";
-    let newCell1 = document.createElement("div");
-    newCell1.innerHTML = "<button onclick='values(1)'>.</button>";
-    newCol.appendChild(newCell1).className = "grid-item";
-    let newCell2 = document.createElement("div");
-    newCell2.innerHTML = "<button onclick='operate()'>=</button>";
-    newCol.appendChild(newCell2).className = "grid-item";
-    let newCell3 = document.createElement("div");
-    newCell3.innerHTML = "<button onclick='operators(1)'>+</button>";
-    newCol.appendChild(newCell3).className = "grid-item";
+    else if(input2!="")
+    {
+        input2 = input2.slice(0,-1);
+    }
+    else
+    {
+        input1 = input1.slice(0,-1);
+    }
 }
 
 function values(num)
 {
-    if(input1==-1)
+    if(input2 == "")
     {
-        input1=num;
-        textbox.value += input1;
+        if(input3!="")
+        {
+            clear1();
+        }
+        input1 += num;
+        console.log("input1: "+input1);
+        textbox.value += num;
     }
     else
     {
-        input3=num;
-        textbox.value += input3;
+        input3 += num;
+        console.log("input3: "+input3);
+        textbox.value += num;
     }
 }
 
 function operators(opp)
 {
-    if(opp==1)
-        opp="+";
-    if(opp==2)
-        opp="*";
-    if(opp==3)
-        opp='/';
-    if(opp==4)
-        opp="-";
     input2=opp;
-    textbox.value += input2;
+    console.log("operator: "+ opp)
+    textbox.value += opp;
 }
-
-makeCal(3,3);
 
 function addition(num1,num2)
 {
@@ -147,4 +107,9 @@ function division(num1,num2)
 function multiplication(num1,num2)
 {
     return Number(num1)*Number(num2);
+}
+
+function percentage(num1,num2)
+{
+    return Number(num1)*100/Number(num2);
 }
